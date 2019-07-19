dd507a292295d6da17899ed4df00495d0e6699a7
Razan Hajja <razan.alhajjeh@hotmail.com>
2019-07-19 19:19:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function setBestCustomer() {
 function overwriteBestCustomer() {
   return bestCustomer = 'maybe bob' ;
 }
-let leastFavoriteCustomer = ''
+let leastFavoriteCustomer = '';
 function changeLeastFavoriteCustomer() {
-
+return leastFavoriteCustomer ;
 }
